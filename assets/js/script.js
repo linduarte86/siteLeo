b1b411a0c05d9607ded5333625ec2299b57d1a37
linduarte86 <linduarte86@gmail.com>
2023-09-10 14:48:32 -0300

    updade10092023

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+/*Script de funções Javascript utilizados no site
+
+*/
+
+// Função escreve título letra por letra / Ativo função piscar
+const titulo = document.querySelector('h1');
+
+function escreveTitulo(elemento) {
+     const textoArray = elemento.innerHTML.split('');
+      elemento.innerHTML = '';
+      textoArray.forEach((letra, i) => {
+          setTimeout(function () {
+              elemento.innerHTML += letra;
+          }, 50 * i)
+      });
+
+    /*setInterval(() => {
+        elemento.style.visibility = (elemento.style.visibility == 'hidden' ? '' : 'hidden');
+    }, 700);*/
+
+}
+escreveTitulo(titulo);
+// -------------------------------------------------
+
+
+// -------------------------------------------------
---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="96x96" href="./assets/img/logoNav.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- icons -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Style css -->
+    <link href="./assets/css/style.css" rel="stylesheet">
+
+    <!-- Título da landind page -->
+    <title>Dr. Léo: Advogado</title>
+</head>
+
+<!-- Corpo do html -->
+
+<body id="bg">
+
+    <!-- Cabeçalho -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" role="navigation">
+            <a class="navbar-brand col-sm-4" target="_blank" href="https://api.whatsapp.com/send?phone=5583981734959">
+                <img src="./assets/img/logoNav.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                Leomando Cesário - OAB-PB 17.288
+            </a>
+        </nav>
+    </header>
+
+    <!-- Main -->
+    <main class="filter">
+
+        <!-- Seção única -->
+        <section>
+            <div class="container-fluid section1">
+                <h1 class="titulo">GARANTA A PENSÃO DO SEU FILHO!</h1>
+                <div class="separator separator-danger">✻</div>
+                <article id="conteudo">
+                    <p class="h3">Assessoria jurídica com mais de 10 anos de experiência na área do direito de família e
+                        em conseguir a pensão alimentícia que sua/seu filha/filho precisa.</p>
+
+                    <p class="h3">Vou te auxiliar a conseguir o seu e de seu filho o direito reconhecido, para que
+                        receba a pensão alimentícia após a maior idade, mesmo tendo completado 18 anos.</p>
+                </article>
+            </div>
+
+            <a href="https://api.whatsapp.com/send?phone=5583981734959" class="btn btn-success btn-lg btn-block"
+                role="button" aria-pressed="true" target="_blank">Entre Em Contato
+                <i class="fa fa-whatsapp"></i>
+            </a>
+
+        </section>
+
+        <!--<div class="separator separator-danger">✻</div>-->
+
+        <section class="section2">
+            <div class="container-fluid">
+                <h2 class="titulo">Quem Somos</h2>
+                <article id="conteudo">
+                    <p class="h3">O Escritório Leomando Cezário Sociedade Individual de advocacia, esta no mercado a mais
+                        de 10 anos trabalhando para garantir o direito de seus cliente, com um vasta experiência no
+                        direito de família, já ajudei mais de 100 crianças e adolescente a ter sua pensão alimentícia
+                        garantida.
+                        Contamos com profissionais altamente capacitados na matéria, preocupados com a constante
+                        atualização dos seus conhecimentos para garantir o melhor resultado para nossos clientes.</p>
+                </article>
+            </div>
+        </section>
+
+        <!--<div class="separator separator-danger">✻</div>-->
+
+        <section class="section3">
+            <div class="container-fluid suaDuvida">
+                
+            </div>
+        </section>
+
+    </main>
+
+    <!-- Roda Pé da landind page -->
+    <footer class="footer">
+        <p>&copy; 2023 - Leomando Cesário Sociedade Individual de Advocacia / CNPJ-51.602.446/0001-27</p>
+    </footer>
+
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+
+    <script src="./assets/js/script.js"></script>
+
+    <!-- Símbolo Whatsapp -->
+    <a class="linkZap" href="https://api.whatsapp.com/send?phone=5583981734959" target="_blank">
+        <i class="fa fa-whatsapp">
+    </a>
+
+</body>
+
+</html>
